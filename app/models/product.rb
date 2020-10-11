b0f0961248b7df80476ae42e17fe3c793eb3ef4f
keddo <adu.kedir.abdurahman@gmail.com>
2020-10-11 17:39:58 +0300

    modify the default validation error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     validates :title, :image_url, :description, presence: true
-    validates :title, uniqueness: true 
+    validates :title, uniqueness: true, length: { minimum: 10, message: 'title should be at least 5 chars' }
 
     validates :image_url, allow_blank: true, format: {
                 with: %r{\.(gif|jpg|png)\z}i,
